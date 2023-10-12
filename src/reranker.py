fed591f73426d7c1d40f26cf70b8a004a67f8c35
Michael Ehme <mc.ehme@gmail.com>
2023-10-11 23:46:20 -0400

    WIP ReRanker

--- src/reranker.py
+++ src/reranker.py
@@ -1,7 +1,16 @@
+from transformers import AutoModelForSequenceClassification,AutoTokenizer, pipeline
+
 class BertReRanker():
     def __init__(self, threshold):
         self.threshold = threshold
-    def pickAcceptable(passages, query):
-        pass
+        self.model = AutoModelForSequenceClassification.from_pretrained("amberoad/bert-multilingual-passage-reranking-msmarco")
+        self.tokenizer = AutoTokenizer.from_pretrained("amberoad/bert-multilingual-passage-reranking-msmarco")
+    
+
+    def pickAcceptable(self, passages, query):
+        tokens = self.tokenizer.tokenize(query)
+        for passage in passages:
+            passage_tokens = self.tokenizer.tokenize(passage)
+            self.model.generate()
     def pickBest(passages, query):
         pass
