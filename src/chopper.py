ff7beee42681143b309c98c6c00b024e0263f666
Michael Ehmec:/Users/mcehm/Documents/KSU/Fall 2023/NLP/Project/NLP-LIVE-QA/venv/Scripts/python.exe <mc.ehme@gmail.com>
2023-10-12 15:35:55 -0400

    Fixing chopper

--- src/chopper.py
+++ src/chopper.py
@@ -1,12 +1,17 @@
 from transformers import AutoTokenizer
-import math
 class Chopper():
     def __init__(self, max_passage):
         self.max_passage = max_passage
         self.tokenizer = AutoTokenizer.from_pretrained("amberoad/bert-multilingual-passage-reranking-msmarco")
 
     def chop(self, doc, query):
-        pass
+        query_size = len(self.tokenizer.tokenize(query))
+        with open(doc, 'r') as f:
+            text = f.read()
+            text_tokens = self.tokenizer.tokenize(text)
+            n = self.max_passage - query_size
+            passages = [text_tokens[i:i+n] for i in range(0, len(text_tokens), n)]
+        return ' '.join(passages)
 
 
     def chopAll(self, docs, query):
